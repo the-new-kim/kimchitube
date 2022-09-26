0fd775f807011cd39a81e37ad85b9994d47da709
the-new-kim <jonkim0309@gmail.com>
2022-09-26 14:59:51 +0900

    css light effects test

--- src/middlewares.js
+++ src/middlewares.js
@@ -25,7 +25,7 @@ const s3VideoUploader = multerS3({
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.siteTitle = "KIMCHI TUBE";
+  res.locals.siteTitle = "Kimchi Tube";
   res.locals.loggedInUser = req.session.user;
   res.locals.isHeroku = isHeroku;
   res.locals.videoMixinTypes = {
